1eae2463737ad2d413ce617d211da882785b2c3c
wim0 <willdmontblanc@gmail.com>
2024-11-09 23:29:54 -0300

    cambios en component button

--- components/Button.tsx
+++ components/Button.tsx
@@ -16,7 +16,7 @@ export default function Button({ label, theme }: Props) {
         ]}>
         <Pressable
           style={[styles.button, { backgroundColor: "#fff" }]}
-          onPress={() => alert("You pressed a button.")}>
+          onPress={() => alert("Has presionado un botón.")}>
           <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
           <Text style={[styles.buttonLabel, { color: "#25292e" }]}>{label}</Text>
         </Pressable>
