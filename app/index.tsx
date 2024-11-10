ea1efaccbedeb0d42765bb6f7c68eb006e729738
wim0 <willdmontblanc@gmail.com>
2024-11-09 22:33:40 -0300

    iniciación final del projecto, con core components

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      /* @tutinfo This used to say "Edit app/index.tsx to edit this screen". Now it says, "Home
+      screen". */
+      <Text style={styles.text}>Home screen</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
+  },
+});
